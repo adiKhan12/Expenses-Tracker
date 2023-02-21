6d49052016e05a9b8ccac11946ecc28ab8bfd9bd
Adnan Khan <Ghories555@gmail.com>
2023-02-21 02:41:21 +0100

    added login/logout auth

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine, Column, Integer, String, Float, Date
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
-
+from sqlalchemy import Column, Integer, String
+from flask_login import UserMixin
 # Create the engine for the SQLite database
 engine = create_engine("sqlite:///expenses.db", echo=True)
 
@@ -23,5 +24,16 @@ class Expense(Base):
     def __repr__(self):
         return f"<Expense(id={self.id}, name='{self.name}', amount={self.amount}, date='{self.date}')>"
 
+# Define the User model
+class User(Base, UserMixin):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(50), unique=True, index=True)
+    password = Column(String(50))
+    role = Column(String(20), default='user')
+
+    def is_active(self):
+        return True
 # Create the tables in the database
 Base.metadata.create_all(engine)
+
--- run.py
+++ run.py
@@ -1,11 +1,79 @@
 from flask import Flask, render_template, request, redirect, url_for
 from expense_tracker import ExpenseTracker
+from flask_login import login_user, logout_user, login_required
+from flask_login import LoginManager, UserMixin
+from database import Session, User
+from flask import flash
+from uuid import uuid4
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = str(uuid4())
+
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 tracker = ExpenseTracker()
 
+@login_manager.user_loader
+def load_user(user_id):
+    session = Session()
+    user = session.query(User).filter(User.id == int(user_id)).first()
+    session.close()
+    return user
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    return redirect('/login')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        register = request.form.get('register')
+        session = Session()
+
+        # Check if the user is registering
+        if register:
+            # Check if the username is already taken
+            if session.query(User).filter_by(username=username).first() is not None:
+                flash('The username is already taken. Please choose a different one.')
+                session.close()
+                return redirect('/login')
+
+            # Create a new user with the provided username and password
+            password_hash = generate_password_hash(password)
+            user = User(username=username, password=password_hash)
+            session.add(user)
+            session.commit()
+
+            flash('You have successfully registered. Please log in to continue.')
+            session.close()
+            return redirect('/login')
+
+        # Check if the username and password are correct
+        user = session.query(User).filter_by(username=username).first()
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            session.close()
+            return redirect('/')
+        else:
+            flash('Invalid username or password')
+            session.close()
+            return render_template('login.html')
+
+    else:
+        return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect('/login')
+
 @app.route("/")
+@login_required
 def index():
     start_date = request.args.get('start_date')
     end_date = request.args.get('end_date')
@@ -17,6 +85,7 @@ def index():
     return render_template('index.html', expenses=expenses)
 
 @app.route("/add", methods=["GET", "POST"])
+@login_required
 def add():
     if request.method == "POST":
         name = request.form["name"]
@@ -28,11 +97,13 @@ def add():
     return render_template("add.html")
 
 @app.route("/delete/<int:id>", methods=["POST"])
+@login_required
 def delete(id):
     tracker.delete_expense(id)
     return redirect(url_for("index"))
 
 @app.route("/edit/<int:id>", methods=["GET", "POST"])
+@login_required
 def edit(id):
     expense = tracker.get_expense(id)
     if request.method == "POST":
